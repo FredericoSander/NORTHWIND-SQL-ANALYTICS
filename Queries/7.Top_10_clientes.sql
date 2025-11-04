0e07da0c5b774bf09d6a9bdde7f083c28e03e5d3
Frederico Sander <sanderfn@hotmail.com>
2025-11-04 15:01:43 -0300

    Adicionando a consulta Queries/7.Top_10_clientes.sql

---
+++ Queries/7.Top_10_clientes.sql
@@ -0,0 +1,10 @@
+-- 7. Quais são os 10 clientes com maior valor total de compras (TOP 10 Total Sales)?
+
+SELECT c.company_name,
+	SUM((od.unit_price * od.quantity)*(1 - od.discount))AS Total_vendas
+FROM customers c
+INNER JOIN orders o ON o.customer_id = c.customer_id
+INNER JOIN order_details od ON od.order_id = o.order_id
+GROUP BY c.company_name
+ORDER BY Total_vendas DESC 
+LIMIT 10;
